1b0bd80bc6b66ca6576a38c1a36364efc98a4db0
prasys <pradeesh@prasys.info>
2019-12-16 09:42:47 +1300

    Update train.py

--- train.py
+++ train.py
@@ -79,7 +79,7 @@ def train_model(data_folder, data_name, level, model_name, is_aspect_term=True):
         for i in range(len(train_input)):
             train_combine_valid_input.append(train_input[i] + valid_input[i])
         train_combine_valid_label = train_label + valid_label
-        model.train(train_combine_valid_input, train_combine_valid_label, test_input, test_label,isResampled)
+        model.train(train_combine_valid_input, train_combine_valid_label, test_input, test_label)
         # model.train(train_combine_valid_input, train_combine_valid_label, test_input, test_label)
 
         elapsed_time = time.time() - start_time

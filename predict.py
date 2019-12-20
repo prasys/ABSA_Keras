9511505ff6309f31b20b9e7115453a883b702dbf
prasys <pradeesh@prasys.info>
2019-12-20 16:08:00 +1300

    temp workaround

--- predict.py
+++ predict.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     config.exp_name += '_elmo_alone_{}_mode_{}_{}'.format(config.use_elmo_alone, config.elmo_output_mode,
                                                           'update' if config.elmo_trainable else 'fix')
     model = loadModel('alta2', 'twitter', 'word', 'td_lstm') # pick when model to load and to do the test
-    predict_input = load_input_data('output', 'train', config.level, config.use_text_input, config.use_text_input_l,
+    predict_input = load_input_data('output', 'test', config.level, config.use_text_input, config.use_text_input_l, #temp workaround
                              config.use_text_input_r, config.use_text_input_r_with_pad, config.use_aspect_input,
                              config.use_aspect_text_input, config.use_loc_input, config.use_offset_input,
                              config.use_mask)

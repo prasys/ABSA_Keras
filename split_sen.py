d18617677a6315daec412678e000dc83c81b23e7
prasys <pradeesh@prasys.info>
2020-01-08 11:01:28 +1300

    Update split_sen.py

--- split_sen.py
+++ split_sen.py
@@ -73,7 +73,7 @@ elif ('xlsx' in file_path):
 df['Comment'] = df['Comment'].str.lower() # make it lower
 df['Comment'] = df['Comment'].apply(scrub_words) #clean up
 df['Comment'].apply(doItAll)
-b = pd.DataFrame.from_dict(list(c.items()), columns=['Word', 'Count']) #counter done
+b = pd.DataFrame.from_dict(c,orient='index',columns={'word','count'}) #counter done
 b.to_csv('count.csv',index=False)
 
 
